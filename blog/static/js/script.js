639e4d648d401684de3f9ab91af1c5e631d7254d
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-04-09 09:37:16 +0530

    +js 1

---
+++ blog/static/js/script.js
@@ -0,0 +1,18 @@
+let form = document.getElementById('blog_form');
+let pages = document.getElementsByClassName('page');
+
+
+for(let i=0; i < pages.length; i++){
+    pages[i].addEventListener('click', func);
+
+    function func(e){
+        e.preventDefault();
+
+        let page = this.dataset.page;
+
+        form.innerHTML += `<input type='text' name="page" value=${page} hidden>`;
+
+        form.submit();
+    }
+}
+
